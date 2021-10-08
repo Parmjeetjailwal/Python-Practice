3300e2f1111076e0d35fedd279667054e765aa97
Parmjeet Jailwal <vermaparmjeet@gmail.com>
2021-10-08 22:14:00 +0530

    Operators in python

---
+++ operators.py
@@ -0,0 +1,73 @@
+# Arithmetic Operators
+# Examples of Arithmetic Operator
+a = 7
+b = 4
+
+# Addition of numbers
+add = a + b
+
+# Subtraction of numbers
+sub = a - b
+
+# Multiplication of number
+mul = a * b
+
+# Division(float) of number
+div1 = a / b
+
+# Division(floor) of number
+div2 = a // b
+
+# Modulo of both number
+mod = a % b
+
+# Power
+p = a ** b
+
+# print results
+print(add)
+print(sub)
+print(mul)
+print(div1)
+print(div2)
+print(mod)
+print(p)
+
+
+# Assignment Operators
+c = 34
+# c -= 12
+# c *= 12
+# c /= 12
+print(c)
+
+
+# Comparison Operators
+e = 20
+f = 31
+
+# e > f is False
+print(e > f)
+
+# e < f is True
+print(e < f)
+
+# e == f is False
+print(e == f)
+
+# e != f is True
+print(e != f)
+
+# e >= f is False
+print(e >= f)
+
+# e <= f is True
+print(e <= f)
+
+
+# Logical Operators
+bool1 = True
+bool2 = False
+print("The value of bool1 and bool2 is", (bool1 and bool2))
+print("The value of bool1 or bool2 is", (bool1 or bool2))
+print("The value of not bool2 is", (not bool2))
