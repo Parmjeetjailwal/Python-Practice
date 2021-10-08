eb6e0f6bd47808f57af14e136b02cd27639d927b
Parmjeet Jailwal <vermaparmjeet@gmail.com>
2021-10-08 21:48:23 +0530

    adding variable.py

---
+++ variable.py
@@ -0,0 +1,22 @@
+# Variables are containers for storing data values
+
+# Examples of variables
+p = 'Parm'
+j = "Jeet"
+b = 1995
+c = 19.95
+d = True
+
+# Printing the variables
+print(p)
+print(j)
+print(b)
+print(c)
+print(d)
+
+# Printing the type of variables
+print(type(p))
+print(type(j))
+print(type(b))
+print(type(c))
+print(type(d))
