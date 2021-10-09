778349abcbd7c3680a843c3b857a6c0802da4fa0
Parmjeet Jailwal <vermaparmjeet@gmail.com>
2021-10-09 20:49:04 +0530

    conversion of one data type into another data type

---
+++ casting.py
@@ -0,0 +1,21 @@
+# conversion of one data type to another data type(if possible) is called casting in python
+a = 34 
+print(a)
+print(type(a))
+
+b = 4.5
+print(int(b))
+print(type(b))
+
+c = "45"
+print(c)
+print(float(c))
+
+d = "string"
+print(int(d))
+
+
+# e = "35fgrfg34"
+# e = int(e)
+# print(type(e))
+# print(e + 6)
