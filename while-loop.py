54d89d179952a13ae9bd526ab5574e9075a28ea6
Parmjeet Jailwal <vermaparmjeet@gmail.com>
2021-10-17 23:08:38 +0530

    adding while-loop.py

---
+++ while-loop.py
@@ -0,0 +1,17 @@
+# while loop = a statement that will execute it's block of code,
+#             as long as it's condition remains ture
+
+# First Method
+name = ""
+while len(name) == 0:
+    name = input("Enter your name: ")
+
+print("Hello "+name)
+
+# Second Method
+# name = None
+
+# while not name:
+#     name = input("Enter your name: ")
+
+# print("Hello "+name)
