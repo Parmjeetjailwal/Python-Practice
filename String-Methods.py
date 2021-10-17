15a3a2e08e1e0295de64928202b7372894ce950f
Parmjeet Jailwal <vermaparmjeet@gmail.com>
2021-10-17 22:10:26 +0530

    Some basic string methods in python

---
+++ String-Methods.py
@@ -0,0 +1,12 @@
+Name = "Parmjeet Jailwal"
+
+print(len(Name))
+print(Name.find("a"))
+print(Name.capitalize())
+print(Name.upper())
+print(Name.lower())
+print(Name.isdigit())
+print(Name.isalpha())
+print(Name.count())
+print(Name.replace("a" , "o"))
+print(Name*3)
