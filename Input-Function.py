6573f8708a72aec6e5c5a8a3c098b156c912b099
Parmjeet Jailwal <vermaparmjeet@gmail.com>
2021-10-17 22:28:34 +0530

    Modifie input-function.py

--- Input-Function.py
+++ Input-Function.py
@@ -1,3 +1,11 @@
 a = input("Enter a number: ")
 b = int(a) # Convert a to an Integer(if possible)
 print(type(b))
+
+name = input("What is your name?: ")
+age = int(input("How old are you?: "))
+height = float(input("How tall are you?: "))
+
+print("Hello " +name)
+print("You are "+str(age)+" years old")
+print("You are "+str(height)+"cm tall")
