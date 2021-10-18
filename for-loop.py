cc2e98fd0dda07ae5985fbe806b547d9b0c5dfd2
Parmjeet Jailwal <vermaparmjeet@gmail.com>
2021-10-18 21:03:57 +0530

    adding for-loop.py

---
+++ for-loop.py
@@ -0,0 +1,22 @@
+import time
+#  for loop =  A statement that will execute it's block of code
+#              a limited amount of times
+#
+#              while loop = unlimited
+#              for loop  = limited
+#
+
+# for i in range(10):
+#     print(i+1)
+
+# for i in range(50, 100+1, 2):
+#     print(i)
+
+# for i in "Parmjeet Jailwal":
+#     print(i)
+
+# Count down 10 to 0
+for seconds in range(10,0,-1):
+    print(seconds)
+    time.sleep(1)
+print("Happy New Year!")
