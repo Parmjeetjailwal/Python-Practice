a8b9eda091b9d915cb955a088b9df84ccfbba587
Parmjeet Jailwal <vermaparmjeet@gmail.com>
2021-10-19 21:52:41 +0530

    adding indexing.py

---
+++ indexing.py
@@ -0,0 +1,14 @@
+# index operator [] = gives access to a sequence's element (str,list,tuples)
+
+name = "parmjeet Jailwal!"
+
+# if(name[0].islower()):
+#     name = name.capitalize()
+
+first_name = name[:8].upper()
+last_name = name[9:].lower()
+last_character = name[-1]
+
+print(first_name)
+print(last_name)
+print(last_character)
