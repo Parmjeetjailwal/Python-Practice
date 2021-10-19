9922d3df252b03dde8669225adef4a4ecc3038da
Parmjeet Jailwal <vermaparmjeet@gmail.com>
2021-10-19 10:26:28 +0530

    adding sets.py

---
+++ sets.py
@@ -0,0 +1,19 @@
+# set = collection which is unordered, unindexed. No duplicate values
+
+utensils = {"fork", "spoon", "knife"}
+dishes = {"bowl", "plate", "cup", "knife"}
+
+# utensils.add("napkin")
+# utensils.remove("fork")
+# utensils.clear()
+# utensils.update(dishes)
+# print(utensils.difference(dishes))
+print(utensils.intersection(dishes))
+
+# for i in utensils:
+#     print(i)
+
+# dinner_table = utensils.union(dishes)
+
+# for i in dinner_table:
+#     print(i)
