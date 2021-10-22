214f383778a4bbbc61bb04a1e1ba800edce137f7
Parmjeet Jailwal <vermaparmjeet@gmail.com>
2021-10-22 23:28:54 +0530

    adding file_detection.py

---
+++ file_detection.py
@@ -0,0 +1,12 @@
+import os
+
+path = input("Enter path of a file or directory: ")
+
+if os.path.exists(path):
+    print("That location exists!")
+    if os.path.isfile(path):
+        print("That is a file")
+    elif os.path.isdir(path):
+        print("That is a directory!")
+else:
+    print("That location doesn't exist!")
