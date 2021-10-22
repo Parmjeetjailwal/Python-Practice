bea56d513a233d33e62fe4fdbbb3596bc4cd15d5
Parmjeet Jailwal <vermaparmjeet@gmail.com>
2021-10-22 22:41:00 +0530

    adding string_format.py

---
+++ string_format.py
@@ -0,0 +1,14 @@
+# str.format() = optional method that gives users
+#                more control when displaying output
+
+# pi = 3.14159
+
+
+# print("The numper pi is {:.3f}".format(pi))
+
+number = 1000
+print("The numper  is {:,}".format(number))
+print("The numper  is {:b}".format(number))
+print("The numper  is {:o}".format(number))
+print("The numper  is {:x}".format(number))
+print("The numper  is {:E}".format(number))
