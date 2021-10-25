adaae6284bddf6ecd0dd21e0fa08364b72719604
Parmjeet Jailwal <vermaparmjeet@gmail.com>
2021-10-25 22:50:06 +0530

    adding modules.py

---
+++ modules.py
@@ -0,0 +1,10 @@
+# module = a file containing python code. May contain functions, classes etc.
+# used with modular programming, which is to separate a program into parts
+
+# import messages
+# import messages as msg
+# from messages import hello,bye
+# from messages import * (this one is dangerous)
+
+
+help("modules")
