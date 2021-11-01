8946e752aeaed8dfad5df236df8a425169f2db8b
Parmjeet Jailwal <vermaparmjeet@gmail.com>
2021-11-01 21:11:19 +0530

    adding walrus-operator.py

---
+++ walrus-operator.py
@@ -0,0 +1,21 @@
+# walrus operator :=
+
+# added in Python 3.8
+# assignment experssion also known as walrus operator
+# assigns values to variables as part of a larger expression
+
+# happy = True
+# print(happy)
+
+# print(happy := True)
+
+# food = list()
+# while True:
+#     food = input("What food do you like?: ")
+#     if food == "quit":
+#         break
+#         food.append(food)
+
+foods = list()
+while food := input("What food do you like?: ") != "quit":
+    foods.append(food)
